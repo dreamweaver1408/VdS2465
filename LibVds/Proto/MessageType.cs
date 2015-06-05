7e01457c54982faca167415bdb01db36f18f96f7
dreamweaver1408 <alex@alex-online.co.uk>
2015-06-05 22:40:52 +0200

    Added MessageTypes
    Message types identify the kind of message transmitted, e.g. intrusion
    alarm, tamper etc.

---
+++ LibVds/Proto/MessageType.cs
@@ -0,0 +1,11 @@
+﻿namespace LibVds.Proto
+{
+    public enum MessageType
+    {
+        Meldung_Ein = 0x00,
+        Meldung_Aus = 0x80,
+        Brandmeldung_Ausgeloest = 0x10,
+        Brandmeldung_Zurueckgesetzt = 0x90,
+
+    }
+}
